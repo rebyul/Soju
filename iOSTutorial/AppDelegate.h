bab747127380b33b4bf2263c78ed08c4d979d547
rebyul <donghan1991@hotmail.com>
2014-11-30 02:48:58 +1300

    Initial Commit

---
+++ iOSTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTutorial
+//
+//  Created by KimDonghan on 30/11/14.
+//  Copyright (c) 2014 Concepti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSTutorial
+//
+//  Created by KimDonghan on 30/11/14.
+//  Copyright (c) 2014 Concepti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
